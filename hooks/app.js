7c40cc50d0649080489402e35dc1ff8710d13781
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-04-05 20:45:00 +0100

    updaed hooks

--- hooks/app.js
+++ hooks/app.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/webhook', secret: '123456' })
-var exec = require('child_process').exec;
+var exec = require('child_process').exec
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
@@ -15,8 +15,8 @@ handler.on('error', function (err) {
 })
 
 handler.on('push', function (event) {
-  console.log('there has been an update')
-  exec("../deploy.sh").stdout.on('data', function(message){
-    console.log(message);
-  });
+  console.log('Received a push event for %s to %s')
+  exec('../deploy.sh').stdout.on('data', function (log) {
+    console.log(log)
+  })
 })
