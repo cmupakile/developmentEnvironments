4a6bb7d8d008f8b053f16ca474733802d7189b99
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-04-05 17:18:06 +0100

    install script update.

---
+++ development_environment/install_script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# update the sources list
+sudo apt-get update -y
+
+# upgrade any plackages available
+sudo apt-get upgrade -y
+
+# install git
+sudo apt-get install git -y
+
+
+# install npm
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install pm2
+sudo npm install pm2 -g
+###############################
+
+# going to this directory
+cd ~/app
+
+#Download the curl and nginx
+sudo apt-get install curl
+sudo apt-get install nginx
+
+
+
+# change directory
+
+sudo rm -r /etc/nginx/sites-available/default
+
+cd ~/
+          # /home/ubuntu/environment --- this is a safer pathfinder
+ sudo cp /home/ubuntu/developmentEnvironment/nginx.default /etc/nginx/sites-available
+
+ # sudo mv /etc/nginx/sites-available/nginx.default /etc/nginx/sites-available/default
+
+# restart the server
+sudo service nginx restart
+
+echo 'script working'
