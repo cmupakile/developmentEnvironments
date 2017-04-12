5a7064aad2f93b08cd1a5d03da70f5df96971594
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-04-12 10:50:34 +0100

    default.rb fix. TDD fix. pm2. node. Test fix.

--- cookbooks/node-server/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/node-server/test/integration/default/serverspec/default_spec.rb
@@ -25,7 +25,7 @@ describe package('nodejs') do
 end
 
 describe package('pm2') do
-	it { should be_installed.by('nodejs::nodejs_from_package') }
+	it { should be_installed.by('npm') }
 end
 
 # describe package('npm') do
